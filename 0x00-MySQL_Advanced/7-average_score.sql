249766940cfc33e375803d1cb497a4afb0db2abd
Simamnkele <smambo85@gmail.com>
2024-03-21 18:07:58 +0000

    Fixed logic errors

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,7 +1,7 @@
 -- Creates a stored procedure 'ComputeAverageScoreForUser'
 -- that computes and stores average score for a student.
-DROP PROCEDURE ComputeAverageScoreForUser (user_id INT)
-DELIMITER
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser (user_id INT)
 BEGIN
 	DECLARE total_score INT DEFAULT 0;
